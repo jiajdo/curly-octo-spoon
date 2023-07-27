3fdd082b07ff1cec9dc7525b8229c9402f34bce3
Jia <jovsofjdo@gmail.com>
2023-07-27 10:09:55 -0700

    Removed unnecessary comment in changeTitleColor function

--- recipe/script.js
+++ recipe/script.js
@@ -6,6 +6,4 @@ function changeTitleColor() {
     const exampleStyle = document.getElementById("title").style;
     flag ? exampleStyle.color = "red" : exampleStyle.color = "black";
     flag = !flag;
-
-    //recipeTitle.style.color = "red"
 } 
