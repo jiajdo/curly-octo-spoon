9200cc58e00379f28a3a08a4e7e66272f935e578
Jia <jovsofjdo@gmail.com>
2023-08-04 12:54:16 -0700

    Adding code challenge questions

--- basicjspractice/basicjspractice.js
+++ basicjspractice/basicjspractice.js
@@ -1,1 +1,22 @@
-gitr
+// Write a function below that accepts a number as the only argument.
+// The function should determine whether the number is divisible by 3,
+// and return either true or false.
+// Choose names that make the code easy to understand.
+//
+// Note:  None of the review # problems have tests included, but feel free to write your own.
+
+// Write a function that takes a string as the argument.
+// The function should return a number that is the number of characters in the string.
+// Choose names that make the code easy to understand.
+
+// 1. Create an array of numbers, (ex: [1, 2, 3])
+// 2. Without using any math operators, write 5 statements:
+//   - Return the first item in the array (ex: 1)
+//   - Return the last item in the array (ex: 3)
+//   - Add a new item to the end of the array (ex: [1,2,3,4])
+//   - Add an array to the beginning of the array. (ex: [["a","b","c"],1,2,3,4])
+//   - Return the first item from the first item in the array. (ex: "a")
+
+// Create a function that calculates how many minutes have elapsed from midnight until right now.
+// The function should still return an accurate answer,
+// even if you ran it a few minutes or hours later.
