a093df581c3eeafc59239fb2b1a695764be98b21
Jia <jovsofjdo@gmail.com>
2023-08-10 23:38:04 -0700

    Deleted snake.js. Wrote draw function for snake

--- game/index.js
+++ game/index.js
@@ -1,15 +1,45 @@
+let SNAKE_SPEED = 2
+let snakeBody = [{x:11, y:11}]
 let lastRenderTime = 0
-const SNAKE_SPEED = 2
+let gameBoard 
+
+document.addEventListener('DOMContentLoaded', function(){
+    gameBoard = document.getElementById('game-board')
+
 
 function main(currentTime){
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
-    console.log('render')
+    
     lastRenderTime = currentTime
+
+    update()
+    draw()
     
 }
 
 window.requestAnimationFrame(main)
+
+function update(){
+ console.log('update snake')
+}
+
+draw()
+
+})
+
+function draw(){
+
+    snakeBody.forEach(segment => {
+       const snakeElement = document.createElement('div')
+       snakeElement.style.gridRowStart = segment.x
+       snakeElement.style.gridColumnStart =  segment.y
+       snakeElement.classList.add('snake')
+       gameBoard.appendChild(snakeElement)
+    })
+   }
+
+
 
