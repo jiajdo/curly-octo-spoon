d1a667c4aa5056ea897d75bab03dbb9882f0e1aa
Jia <jovsofjdo@gmail.com>
2023-08-11 00:25:05 -0700

    Added event listener for arrow keys

--- game/index.js
+++ game/index.js
@@ -1,51 +1,77 @@
 let SNAKE_SPEED = 2
 const snakeBody = [
-    {x:10, y:11},
-    {x:11, y:11},
+    { x: 10, y: 11 },
+    { x: 11, y: 11 },
 
 ]
 let lastRenderTime = 0
-let gameBoard 
+let gameBoard
+let inputDirection = { x: 0, y: 0 }
+let lastInputDirection = { x:0, y:0 }
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     gameBoard = document.getElementById('game-board')
 
 
-function main(currentTime){
-    window.requestAnimationFrame(main)
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+    function main(currentTime) {
+        window.requestAnimationFrame(main)
+        const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+        if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
-    
-    lastRenderTime = currentTime
 
-    update()
+        lastRenderTime = currentTime
+
+        update()
+        draw()
+
+    }
+
+   // window.requestAnimationFrame(main)
+    window.addEventListener('keydown', e => {
+        switch (e.key) {
+            case 'ArrowUp':
+                if (lastInputDirection.y !== 0) break
+                inputDirection = { x: 0, y: -1 }
+                break
+            case 'ArrowDown':
+                if (lastInputDirection.y !== 0) break
+                inputDirection = { x: 0, y: 1 }
+                break
+            case 'ArrowLeft':
+                if (lastInputDirection.x !== 0) break
+                inputDirection = { x: -1, y: 0 }
+                break
+            case 'ArrowRight':
+                if (lastInputDirection.x !== 0) break
+                inputDirection = { x: 1, y: 0 }
+                break
+        }
+    })
+function getInputDirection(){
+    lastInputDirection = inputDirection
+    return inputDirection
+}
+    function update() {
+        for (let i = snakeBody.length - 2; i >= 0; i--) {
+            snakeBody[i + 1] = { ...snakeBody[i] }
+        }
+         snakeBody[0].x += inputDirection.x
+         snakeBody[0].y += inputDirection.y
+    }
+
+
+    function draw() {
+        gameBoard.innerHTML = ''
+        snakeBody.forEach(segment => {
+            const snakeElement = document.createElement('div')
+            snakeElement.style.gridRowStart = segment.y
+            snakeElement.style.gridColumnStart = segment.x
+            snakeElement.classList.add('snake')
+            gameBoard.appendChild(snakeElement)
+        })
+    }
     draw()
     
-}
-
-window.requestAnimationFrame(main)
-
-function update(){
- for (let i = snakeBody.length - 2; i >=0; i--){
-    snakeBody[i + 1] = {...snakeBody[i]}
- }
- snakeBody[0].x += 1
- snakeBody[0].y += 0
-}
-
-
-function draw(){
-    gameBoard.innerHTML = ''
-    snakeBody.forEach(segment => {
-       const snakeElement = document.createElement('div')
-       snakeElement.style.gridRowStart = segment.y
-       snakeElement.style.gridColumnStart =  segment.x
-       snakeElement.classList.add('snake')
-       gameBoard.appendChild(snakeElement)
-    })
-   }
-   draw()
 })
 
 
